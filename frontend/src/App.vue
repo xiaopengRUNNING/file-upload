83bbfdf51135106491fe542428553126581e8bf6
pengxincheng <pengxiaocheng@163.com>
2022-02-15 16:31:27 +0800

    feat: 增加切片上传进度条

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,22 +3,38 @@
     <a-upload :showUploadList="false" :customRequest="customRequest">
       <a-button>Upload</a-button>
     </a-upload>
-    <div>
+    <div class="file-upload-mode">
       是否切片上传：<a-switch
         size="small"
         :checked="isChunk"
         @change="uploadModeChange"
       />
     </div>
-    <div>
-      <a-progress
-        :percent="fileProgress"
-        style="width: 200px"
-        :showInfo="false"
-      ></a-progress>
-    </div>
     <div class="file-upload-progress">
       <div class="progress"></div>
+    </div>
+    <div class="file-chunk-list" v-if="isChunk">
+      <div
+        class="file-chunk-item"
+        v-for="(item, index) in fileChunkList"
+        :key="index"
+      >
+        <div class="chunk-progress"></div>
+        <div v-if="false" class="sk-fading-circle">
+          <div class="sk-circle1 sk-circle"></div>
+          <div class="sk-circle2 sk-circle"></div>
+          <div class="sk-circle3 sk-circle"></div>
+          <div class="sk-circle4 sk-circle"></div>
+          <div class="sk-circle5 sk-circle"></div>
+          <div class="sk-circle6 sk-circle"></div>
+          <div class="sk-circle7 sk-circle"></div>
+          <div class="sk-circle8 sk-circle"></div>
+          <div class="sk-circle9 sk-circle"></div>
+          <div class="sk-circle10 sk-circle"></div>
+          <div class="sk-circle11 sk-circle"></div>
+          <div class="sk-circle12 sk-circle"></div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -34,23 +50,27 @@ const fileProgress = ref();
 const isChunk = ref(true);
 let fileHash = ref('');
 
+const uploadModeChange = checked => {
+  isChunk.value = checked;
+};
+
 const calculateProgress = () => {
-  const ele = document.getElementsByClassName('progress');
+  const ele = document.getElementsByClassName('chunk-progress');
 
   const animation = () => {
     const requestId = window.requestAnimationFrame(animation);
-    ele[0].style.width = `${fileProgress.value}%`;
+    fileChunkList.value
+      .filter(v => v.progress !== 0)
+      .map(v => {
+        ele[v.chunkIndex].style.width = `${v.progress}%`;
+      });
 
-    if (fileProgress.value >= 100) {
+    if (fileChunkList.value.every(v => v.progress === 100)) {
       window.cancelAnimationFrame(requestId);
     }
   };
 
   animation();
-};
-
-const uploadModeChange = checked => {
-  isChunk.value = checked;
 };
 
 /**
@@ -73,7 +93,9 @@ const fileChunk = file => {
   while (cur < file.size) {
     fileChunkList.value.push({
       chunk: file.slice(cur, cur + CHUNK_SIZE),
-      chunkIndex: cur / CHUNK_SIZE
+      chunkIndex: cur / CHUNK_SIZE,
+      status: 'loading',
+      progress: 0
     });
     cur += CHUNK_SIZE;
   }
@@ -165,6 +187,7 @@ const customRequest = e => {
         asyncPool(4, noExistChunkList, handlerChunkUpload).then(() => {
           mergeFileChunk();
         });
+        calculateProgress();
       } else {
         handerFileUpload(e.file, fileHash.value);
         calculateProgress();
@@ -200,16 +223,23 @@ const mergeFileChunk = () => {
  * @param {File} chunk 切片文件
  * @param {Number} index 此切片在切片数组中的索引
  */
-const handlerChunkUpload = (chunk, index) => {
+const handlerChunkUpload = record => {
   const formData = new FormData();
-  formData.append('file', chunk);
+  formData.append('file', record.chunk);
   formData.append('fileHash', fileHash.value);
   formData.append('chunkNum', fileChunkList.value.length);
-  formData.append('chunkIndex', index);
+  formData.append('chunkIndex', record.chunkIndex);
   return request({
     url: 'http://localhost:3001/chunkUpload',
-    data: formData
+    data: formData,
+    onProgress: saveChunkProgress(record)
   });
+};
+
+const saveChunkProgress = chunkRecord => {
+  return e => {
+    chunkRecord.progress = (e.loaded / e.total) * 100;
+  };
 };
 
 /**
@@ -237,7 +267,7 @@ const handerFileUpload = (file, fileHash) => {
 function asyncPool(poolLimit, array, hander) {
   let sequence = [].concat(array);
   let promises = sequence.splice(0, poolLimit).map((item, index) => {
-    return hander(item.chunk, item.chunkIndex).then(() => {
+    return hander(item).then(() => {
       // 异步请求完成后返回在promises中的下标
       return index;
     });
@@ -252,12 +282,10 @@ function asyncPool(poolLimit, array, hander) {
           })
           .then(fastIndex => {
             // 替换已完成的异步请求
-            promises[fastIndex] = hander(curr.chunk, curr.chunkIndex).then(
-              () => {
-                // 继续将下标返回，以便下一次遍历
-                return fastIndex;
-              }
-            );
+            promises[fastIndex] = hander(curr).then(() => {
+              // 继续将下标返回，以便下一次遍历
+              return fastIndex;
+            });
           })
           // 异常捕获
           .catch(err => console.error(err))
@@ -269,7 +297,13 @@ function asyncPool(poolLimit, array, hander) {
     });
 }
 
-function request({ url, method = 'post', data, headers = {} }) {
+function request({
+  url,
+  method = 'post',
+  data,
+  headers = {},
+  onProgress = e => e
+}) {
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
     xhr.open(method, url);
@@ -277,10 +311,7 @@ function request({ url, method = 'post', data, headers = {} }) {
     Object.keys(headers).forEach(key => {
       xhr.setRequestHeader(key, headers[key]);
     });
-    xhr.upload.onprogress = e => {
-      let percentage = e.loaded / e.total;
-      fileProgress.value = percentage * 100;
-    };
+    xhr.upload.onprogress = onProgress;
     xhr.send(data);
     xhr.onload = e => {
       console.log(e);
@@ -296,30 +327,47 @@ function request({ url, method = 'post', data, headers = {} }) {
 </script>
 
 <style lang="less" scoped>
-.file-upload-progress {
-  height: 32px;
-  width: 200px;
-  border: 1px solid black;
-  margin: auto;
-
-  .progress {
-    height: 100%;
-    width: 0;
-    background-color: black;
-  }
-}
 .file-upload-container {
   margin: auto;
   max-width: 66%;
-}
-.file-chunk-list {
-  display: flex;
-  justify-content: center;
 
-  .file-chunk-item {
-    width: 30px;
-    height: 30px;
-    background-color: red;
+  .file-upload-mode {
+    margin: 8px 0px;
+  }
+  .file-upload-progress {
+    height: 32px;
+    width: 200px;
+    border: 1px solid black;
+    margin: auto;
+
+    .progress {
+      height: 100%;
+      width: 0;
+      background-color: black;
+    }
+  }
+  .file-chunk-list {
+    display: flex;
+    flex-wrap: wrap;
+
+    .file-chunk-item {
+      width: 30px;
+      height: 30px;
+      background-color: red;
+      margin: 0px 4px 4px 0px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+
+      .chunk-progress {
+        height: 100%;
+        width: 0%;
+        background-color: blue;
+        position: absolute;
+        left: 0px;
+      }
+    }
   }
 }
 </style>
@@ -332,5 +380,152 @@ function request({ url, method = 'post', data, headers = {} }) {
   text-align: center;
   color: #2c3e50;
   padding-top: 60px;
+}
+
+.sk-fading-circle {
+  width: 24px;
+  height: 24px;
+  position: relative;
+}
+
+.sk-fading-circle .sk-circle {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+}
+
+.sk-fading-circle .sk-circle:before {
+  content: '';
+  display: block;
+  margin: 0 auto;
+  width: 15%;
+  height: 15%;
+  background-color: #333;
+  border-radius: 100%;
+  -webkit-animation: sk-circleFadeDelay 1.2s infinite ease-in-out both;
+  animation: sk-circleFadeDelay 1.2s infinite ease-in-out both;
+}
+.sk-fading-circle .sk-circle2 {
+  -webkit-transform: rotate(30deg);
+  -ms-transform: rotate(30deg);
+  transform: rotate(30deg);
+}
+.sk-fading-circle .sk-circle3 {
+  -webkit-transform: rotate(60deg);
+  -ms-transform: rotate(60deg);
+  transform: rotate(60deg);
+}
+.sk-fading-circle .sk-circle4 {
+  -webkit-transform: rotate(90deg);
+  -ms-transform: rotate(90deg);
+  transform: rotate(90deg);
+}
+.sk-fading-circle .sk-circle5 {
+  -webkit-transform: rotate(120deg);
+  -ms-transform: rotate(120deg);
+  transform: rotate(120deg);
+}
+.sk-fading-circle .sk-circle6 {
+  -webkit-transform: rotate(150deg);
+  -ms-transform: rotate(150deg);
+  transform: rotate(150deg);
+}
+.sk-fading-circle .sk-circle7 {
+  -webkit-transform: rotate(180deg);
+  -ms-transform: rotate(180deg);
+  transform: rotate(180deg);
+}
+.sk-fading-circle .sk-circle8 {
+  -webkit-transform: rotate(210deg);
+  -ms-transform: rotate(210deg);
+  transform: rotate(210deg);
+}
+.sk-fading-circle .sk-circle9 {
+  -webkit-transform: rotate(240deg);
+  -ms-transform: rotate(240deg);
+  transform: rotate(240deg);
+}
+.sk-fading-circle .sk-circle10 {
+  -webkit-transform: rotate(270deg);
+  -ms-transform: rotate(270deg);
+  transform: rotate(270deg);
+}
+.sk-fading-circle .sk-circle11 {
+  -webkit-transform: rotate(300deg);
+  -ms-transform: rotate(300deg);
+  transform: rotate(300deg);
+}
+.sk-fading-circle .sk-circle12 {
+  -webkit-transform: rotate(330deg);
+  -ms-transform: rotate(330deg);
+  transform: rotate(330deg);
+}
+.sk-fading-circle .sk-circle2:before {
+  -webkit-animation-delay: -1.1s;
+  animation-delay: -1.1s;
+}
+.sk-fading-circle .sk-circle3:before {
+  -webkit-animation-delay: -1s;
+  animation-delay: -1s;
+}
+.sk-fading-circle .sk-circle4:before {
+  -webkit-animation-delay: -0.9s;
+  animation-delay: -0.9s;
+}
+.sk-fading-circle .sk-circle5:before {
+  -webkit-animation-delay: -0.8s;
+  animation-delay: -0.8s;
+}
+.sk-fading-circle .sk-circle6:before {
+  -webkit-animation-delay: -0.7s;
+  animation-delay: -0.7s;
+}
+.sk-fading-circle .sk-circle7:before {
+  -webkit-animation-delay: -0.6s;
+  animation-delay: -0.6s;
+}
+.sk-fading-circle .sk-circle8:before {
+  -webkit-animation-delay: -0.5s;
+  animation-delay: -0.5s;
+}
+.sk-fading-circle .sk-circle9:before {
+  -webkit-animation-delay: -0.4s;
+  animation-delay: -0.4s;
+}
+.sk-fading-circle .sk-circle10:before {
+  -webkit-animation-delay: -0.3s;
+  animation-delay: -0.3s;
+}
+.sk-fading-circle .sk-circle11:before {
+  -webkit-animation-delay: -0.2s;
+  animation-delay: -0.2s;
+}
+.sk-fading-circle .sk-circle12:before {
+  -webkit-animation-delay: -0.1s;
+  animation-delay: -0.1s;
+}
+
+@-webkit-keyframes sk-circleFadeDelay {
+  0%,
+  39%,
+  100% {
+    opacity: 0;
+  }
+  40% {
+    opacity: 1;
+  }
+}
+
+@keyframes sk-circleFadeDelay {
+  0%,
+  39%,
+  100% {
+    opacity: 0;
+  }
+  40% {
+    opacity: 1;
+  }
 }
 </style>
